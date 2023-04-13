a182465858e7311cf061873401075212e39b667f
gabriel_combe <gabrielcombe@gmail.com>
2023-04-13 12:47:28 +0200

    Add particle and resample dict

--- particle_filter/__init__.py
+++ particle_filter/__init__.py
@@ -1,1 +1,12 @@
 from .ParticleFilter import ParticleFilter
+from .Particle import ConstAccelParticle2DFixBbox, ConstAccelParticle2DBbox
+from .ResampleMethods import systematic_resample
+
+particle_dict = {
+    'cap2Dfbb': ConstAccelParticle2DFixBbox,
+    'cap2Dbb': ConstAccelParticle2DBbox
+    }
+
+resample_dict = {
+    'systematic': systematic_resample
+    }
