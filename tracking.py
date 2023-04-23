59525965148b56552f2f89d14bf41d9c6c773a61
gabriel_combe <gabrielcombe@gmail.com>
2023-04-23 21:26:05 +0200

    Add comments to tracking.py

--- tracking.py
+++ tracking.py
@@ -1,7 +1,6 @@
 import cv2
 import argparse
 import numpy as np
-from tqdm import trange
 from particle_filter import ParticleFilter, particle_dict, resample_dict
 from utils import slicer_dict, similarity_dict, descriptor_dict
 from detect_init import Model
@@ -46,40 +45,37 @@ def get_opts():
 
     return parser.parse_args()
 
+# Draw Bbox
+def draw_output_frame(frame : np.array, estimate_particle : np.array, color=(0,255,0)):
+    output_image = cv2.rectangle(
+        frame, 
+        (int(estimate_particle[0] - estimate_particle[6]//2), int(estimate_particle[3] - estimate_particle[7]//2)),
+        (int(estimate_particle[0] + estimate_particle[6]//2), int(estimate_particle[3] + estimate_particle[7]//2)),
+        color,
+        thickness=2)
+                    
+    return output_image
+
+# Draw each particles
+def draw_output_particles(frame : np.array, particles : np.array,color=(0,0,255)):
+    for particle in particles[:, 0]:
+        output_image = cv2.circle(frame, (int(particle[0]), int(particle[3])), radius=1, color=color, thickness=1)
+    return output_image
+
+# Draw the mean particle
+def draw_output_mean_particule(frame : np.array, mean_particles : np.array, color=(255,0,0)):
+    output_image = cv2.circle(frame, (int(mean_particles[0]), int(mean_particles[3])), radius=3, color=color, thickness=3)
+    return output_image
+
+
+# Select a cuttlefish to track
 def cuttlefish_picker(init_frame, conf, cuttlefish):
     error = (1-conf) * np.sqrt(((init_frame.shape[1]//2)-cuttlefish[:, 0])**2 + ((init_frame.shape[0]//2)-cuttlefish[:, 1])**2)
     index = np.argmin(error)
     return (conf[index], cuttlefish[index])
 
-def draw_output_frame(frame : np.array, 
-                    estimate_particle : np.array,
-                    color=(0,255,0)):
 
-    output_image = cv2.rectangle(frame, 
-                        (int(estimate_particle[0] - estimate_particle[6]//2), int(estimate_particle[3] - estimate_particle[7]//2)),
-                        (int(estimate_particle[0] + estimate_particle[6]//2), int(estimate_particle[3] + estimate_particle[7]//2)),
-                        color,
-                        2)
-                    
-    return output_image
-    
-    
-def draw_output_particles(frame : np.array, 
-                    particles : np.array,
-                    color=(0,0,255)):
 
-    for particle in particles[:, 0]:
-        output_image = cv2.circle(frame, (int(particle[0]), int(particle[3])), radius=1, color=color, thickness=1)
-    
-    return output_image
-
-def draw_output_mean_particule(frame : np.array, 
-                    mean_particles : np.array,
-                    color=(255,0,0)):
-
-    output_image = cv2.circle(frame, (int(mean_particles[0]), int(mean_particles[3])), radius=3, color=color, thickness=3)
-    
-    return output_image
 
 if __name__ == "__main__":
     # get params
@@ -88,9 +84,13 @@ if __name__ == "__main__":
     # init params
     N = abs(args.N)
     stop = False
+
+    # init Model
     model = Model(
         weights=args.weights, device=args.device, img_size=args.img_size,
         conf_thres=args.conf_thres, iou_thres=args.iou_thres)
+
+    # init functions and classes
     descriptor = descriptor_dict[args.descriptor]
     similarity = similarity_dict[args.similarity]
     resampling = resample_dict[args.resampling]
@@ -121,54 +121,60 @@ if __name__ == "__main__":
 
     # _, init_frame = cap.read()
     # tracked_conf, tracked_cuttlefish = (0.73389, np.array([824.5, 798.5, 203, 151]))
+
+    # Selected Cuttlefish
     print(tracked_conf, tracked_cuttlefish)
 
     # Create video output
     if args.save_video:
         outvid = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, (init_frame.shape[1], init_frame.shape[0]))
 
+    # Create initial position
     init_pos = np.array([[
         [tracked_cuttlefish[0], 0, 0, tracked_cuttlefish[1], 0, 0, tracked_cuttlefish[2], tracked_cuttlefish[3]],
         [(1-tracked_conf), 0.1, 0.01, (1-tracked_conf), 0.1, 0.01, (1-tracked_conf), (1-tracked_conf)]
     ]])
 
+    # Create covariance matrices for prediction and update model
     Q_motion = np.array([[10, 0.25, 0.025, 10, 0.25, 0.025, 10, 8]])
     R = np.array([[.2]])
 
-    # Init Particle filter
+    # Initialize Particle filter
     particle_filter =  ParticleFilter(N, particle_struct, 1, init_pos, init_frame, Q_motion, R, slicer, descriptor, similarity, resampling)
 
-    print(particle_filter.mu)
-    # Loop
+    # Processing loop
     while not stop:
-        ret, current_frame = cap.read() # Read next frame
 
+        # Read a frame
+        ret, current_frame = cap.read()
         if not ret:
             print("Error : Couldn't read frame")
             quit()
 
-        particle_filter.forward(current_frame, 1/fps) # New pass
+        # Perform a pass of the particle filter
+        particle_filter.forward(current_frame, 1/fps)
 
+        # Mean particle
         print(particle_filter.mu)
         print()
+
+        # Draw Bbox and particles on the frame
         output_frame = draw_output_frame(current_frame, particle_filter.mu)
         output_frame = draw_output_particles(output_frame, particle_filter.particles)
         output_frame = draw_output_mean_particule(output_frame, particle_filter.mu)
         cv2.imshow('Track Cuttlefish', output_frame)
 
-        # Write the frame into the file 'output.avi'
+        # Write the frame into the video file
         if args.save_video:
             outvid.write(output_frame)
 
-        # Press Q on keyboard to stop recording
+        # Press Q to stop the particle filter
         if cv2.waitKey(1) & 0xFF == ord('q'):
             stop = True
 
-    # End Loop
-
-    # When everything done, release the video capture and video write objects
+    # Release the video capture and video write objects
     cap.release()
     outvid.release()
     
-    # Closes all the frames
+    # Closes all the windows
     cv2.destroyAllWindows()
