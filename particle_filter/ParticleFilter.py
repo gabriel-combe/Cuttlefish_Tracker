Add new particle, fix bugs, tune covariance matrices

particle_filter/ParticleFilter.py
@@ -3,7 +3,7 @@ from typing import Optional, Tuple
 from .Particle import Particle, ConstAccelParticle2DBbox
 from .ResampleMethods import systematic_resample
 from utils.Descriptors import HOG
-from utils.Slicer import image_resize_slicing, template_image_slicing
+from utils.Slicer import image_resize_slicing
 from utils.Similarity import Bhattacharyya_distance
 
 import cv2
@@ -69,19 +69,19 @@ class ParticleFilter(object):
         # Array of all the trackers and particles
         self.particles: np.ndarray = np.zeros((self.N, self.track_dim, self.state_dim))
         self.particles = self.particle_struct.create_gaussian_particles(self.N, self.track_dim, init_pos[:, 0], init_pos[:, 1])
-        self.prev_particles = self.particles
+        self.prev_particles = np.copy(self.particles)
         
         # Weights of each trackers
         self.weights: np.ndarray = np.ones(self.N)/self.N
         
         # Mean of all the particles for each targets
-        self.mu: np.ndarray = init_pos[0, 0]
+        self.mu: np.ndarray = init_pos[:, 0]
 
         # Standard deviation of all the particles for each targets
-        self.sigma: np.ndarray = init_pos[0, 1]
+        self.sigma: np.ndarray = init_pos[:, 1]
 
         # Set the descriptor function
-        self.descriptor = descriptor((int(self.mu[6]), int(self.mu[7])))
+        self.descriptor = descriptor
 
         # Set the similarity measurement function
         self.similarity = similarity_fn
@@ -90,12 +90,12 @@ class ParticleFilter(object):
         self.slicer = slicer_fn
 
         # Save the descriptor of the best particle at the previous frame
-        template_patch = template_image_slicing(self.prev_frame, self.mu)
-        self.prev_patch_descriptor = self.descriptor.compute(np.array([template_patch]))
+        self.template_patch = self.slicer(np.array([self.mu]), self.prev_frame, self.mu)
+        self.prev_patch_descriptor = self.descriptor.compute(self.template_patch)
 
     # Predict next state for each trackers (prior)
     def predict(self, dt: Optional[float] =1.) -> None:
-        aux_particles = self.particles
+        aux_particles = np.copy(self.particles)
         self.particles = self.particle_struct.motion_model(self.particles, self.Q_motion, self.prev_particles, self.frame_size, dt)
         self.prev_particles = aux_particles
 
@@ -103,26 +103,31 @@ class ParticleFilter(object):
     def update(self, z: np.ndarray) -> None:
         self.prev_frame = z
 
-        image_slice = self.slicer(self.particles, z, self.mu)
+        image_slice = self.slicer(self.particles, self.prev_frame, self.mu)
 
         descriptor_result = self.descriptor.compute(image_slice)
 
         coeff_sim = self.similarity(descriptor_result, self.prev_patch_descriptor)
-
+        
         self.weights = self.particle_struct.measurement_model(coeff_sim, self.R)
+        # self.weights = coeff
 
         self.weights += 1.e-12
         self.weights /= np.sum(self.weights)
+        # self.weights /= np.sum(np.exp(coeff_sim))
+
+        cv2.imshow('best particle', image_slice[np.argmax(self.weights)])
+        cv2.imshow('template particle', self.template_patch[0])
 
     # Computation of the mean and standard deviation of the particles for each targets (estimate)
     def estimate(self) -> tuple[np.ndarray, np.ndarray]:
-        self.mu = np.average(self.particles, weights=self.weights, axis=0)[0]
-        self.sigma = np.average((self.particles - self.mu)**2, weights=self.weights, axis=0)[0]
+        self.mu = np.average(self.particles, weights=self.weights, axis=0)
+        self.sigma = np.average((self.particles - self.mu)**2, weights=self.weights, axis=0)
 
         # Compute the descriptor of the estimate particle
-        template_patch = template_image_slicing(self.prev_frame, self.mu)
-        self.descriptor.update((template_patch.shape[1], template_patch.shape[0]))
-        self.prev_patch_descriptor = self.descriptor.compute(np.array([template_patch]))
+        self.template_patch = self.slicer(np.array([self.mu]), self.prev_frame, self.mu)
+        self.descriptor.update((int(self.mu[0, 6]), int(self.mu[0, 7])))
+        self.prev_patch_descriptor = self.descriptor.compute(self.template_patch)
 
         return (self.mu, self.sigma)
 
@@ -132,8 +137,10 @@ class ParticleFilter(object):
 
     # Perform resample 
     def resample(self, fraction: Optional[float] =1./4.) -> None:
+        print(self.neff())
         if self.neff() < self.N * fraction:
             indexes = self.resample_method(self.weights)
+            print(indexes)
             self.particles[:] = self.particles[indexes]
             self.weights.resize(self.N)
             self.weights.fill(1/self.N)

tracking.py
@@ -11,8 +11,8 @@ def get_opts():
     
     # File args
     parser.add_argument('--filepath', type=str, required=True, help='filepath of the video')
-    parser.add_argument('--video-size', nargs=2, type=int, default=None,
-                        help='Size used to resize the frames of the video')
+    parser.add_argument('--scale-factor', type=float, default=1.,
+                        help='Scale used to resize the frames of the video')
     
     # Particle Filter args
     parser.add_argument('--N', type=int, default=500,
@@ -24,7 +24,7 @@ def get_opts():
                         choices=['brisk', 'sift', 'hog', 'orb', 'color'],
                         help='which descriptor to use')
     parser.add_argument('--similarity', type=str, default='bd',
-                        choices=['bd'],
+                        choices=['bd', 'kpm'],
                         help='which similartiy measure to use')
     parser.add_argument('--resampling', type=str, default='systematic',
                         choices=['systematic', 'residual', 'stratified', 'multinomial'],
@@ -32,6 +32,9 @@ def get_opts():
     parser.add_argument('--slicer', type=str, default='resize',
                         choices=['resize', 'crop'],
                         help='which slicer to use')
+    
+    # Descriptor args
+    parser.add_argument('--nb-features', type=int, default=4000, help='Max number of feature for keypoint descriptors')
     
     # Model args
     parser.add_argument('--weights', nargs='+', type=str, default='weights/cuttlefish_best.pt', help='model.pt path(s)')
@@ -52,14 +55,16 @@ def get_opts():
 
 # Draw Bbox
 def draw_output_frame(frame : np.array, estimate_particle : np.array, color=(0,255,0)):
-    output_image = cv2.rectangle(
-        frame, 
-        (int(estimate_particle[0] - estimate_particle[6]//2), int(estimate_particle[3] - estimate_particle[7]//2)),
-        (int(estimate_particle[0] + estimate_particle[6]//2), int(estimate_particle[3] + estimate_particle[7]//2)),
-        color,
-        thickness=2)
+    output_frame = frame
+    for p in estimate_particle:
+        output_frame = cv2.rectangle(
+            output_frame, 
+            (int(p[0] - p[6]//2), int(p[3] - p[7]//2)),
+            (int(p[0] + p[6]//2), int(p[3] + p[7]//2)),
+            color,
+            thickness=2)
                     
-    return output_image
+    return output_frame
 
 # Draw each particles
 def draw_output_particles(frame : np.array, particles : np.array,color=(0,0,255)):
@@ -69,7 +74,8 @@ def draw_output_particles(frame : np.array, particles : np.array,color=(0,0,255)
 
 # Draw the mean particle
 def draw_output_mean_particule(frame : np.array, mean_particles : np.array, color=(255,0,0)):
-    output_image = cv2.circle(frame, (int(mean_particles[0]), int(mean_particles[3])), radius=3, color=color, thickness=3)
+    for p in mean_particles:
+        output_image = cv2.circle(frame, (int(p[0]), int(p[3])), radius=3, color=color, thickness=3)
     return output_image
 
 
@@ -91,20 +97,11 @@ if __name__ == "__main__":
     seed = args.seed
     video_size = None
     stop = False
-    if args.video_size:
-        video_size = tuple(args.video_size)
 
     # init Model
     model = Model(
         weights=args.weights, device=args.device, img_size=args.img_size,
         conf_thres=args.conf_thres, iou_thres=args.iou_thres)
-
-    # init functions and classes
-    descriptor = descriptor_dict[args.descriptor]
-    similarity = similarity_dict[args.similarity]
-    resampling = resample_dict[args.resampling]
-    slicer = slicer_dict[args.slicer]
-    particle_struct = particle_dict[args.particle]
 
     # Load video
     cap = cv2.VideoCapture(args.filepath)
@@ -114,23 +111,23 @@ if __name__ == "__main__":
     conf = []
     while not len(conf):
         # Read first frame
-        ret, init_frame = cap.read()
+        ret, current_frame = cap.read()
         if not ret:
             print("Error : Couldn't read frame")
             quit()
 
         # Resize if video_size is not None
-        if video_size:
-            init_frame = cv2.resize(init_frame, video_size)
+        if args.scale_factor != 1.:
+            current_frame = cv2.resize(current_frame, (int(current_frame.shape[1]/args.scale_factor), int(current_frame.shape[0]/args.scale_factor)))
 
         # Save image dimensions
-        img_size = (init_frame.shape[1], init_frame.shape[0])
+        img_size = (current_frame.shape[1], current_frame.shape[0])
 
         # Run YOLOV7 to get bounding boxes
-        conf, cuttlefish = model.detect(init_frame)
+        conf, cuttlefish = model.detect(current_frame)
 
         if(len(conf)):
-            tracked_conf, tracked_cuttlefish = cuttlefish_picker(init_frame, conf, cuttlefish)
+            tracked_conf, tracked_cuttlefish = cuttlefish_picker(current_frame, conf, cuttlefish)
 
     # _, init_frame = cap.read()
     # tracked_conf, tracked_cuttlefish = (0.73389, np.array([824.5, 798.5, 203, 151]))
@@ -145,23 +142,34 @@ if __name__ == "__main__":
     # Create initial position
     init_pos = np.array([[
         [tracked_cuttlefish[0], 0, 0, tracked_cuttlefish[1], 0, 0, tracked_cuttlefish[2], tracked_cuttlefish[3]],
-        [(1-tracked_conf)*20, 0.1, 0.01, (1-tracked_conf)*20, 0.1, 0.01, (1-tracked_conf)*20, (1-tracked_conf)*20]
+        [(1-tracked_conf)*40, 0.1, 0.1, (1-tracked_conf)*40, 0.1, 0.1, (1-tracked_conf), (1-tracked_conf)]
     ]])
 
     # Create covariance matrices for prediction and update model
-    Q_motion = np.array([[15, 0.1, 0.01, 10, 0.1, 0.01, 30, 30]])
-    R = np.array([[.01]])
+    Q_motion = np.array([[4, 5, 8, 4, 5, 8, 2, 2]])
+    R = np.array([[.2]])
+
+    # init functions and classes
+    if args.descriptor == 'hog':
+        descriptor = descriptor_dict[args.descriptor]((int(tracked_cuttlefish[2]), int(tracked_cuttlefish[3])))
+    else:
+        descriptor = descriptor_dict[args.descriptor](args.nb_features)
+
+    similarity = similarity_dict[args.similarity]
+    resampling = resample_dict[args.resampling]
+    slicer = slicer_dict[args.slicer]
+    particle_struct = particle_dict[args.particle]
 
     # Initialize Particle filter
     particle_filter =  ParticleFilter(
         N, particle_struct, 1, 
-        init_pos, init_frame, 
+        init_pos, np.copy(current_frame), 
         Q_motion, R, 
         slicer, descriptor, similarity, resampling,
         seed)
 
     # Show Initial particles
-    output_frame = draw_output_frame(init_frame, particle_filter.mu)
+    output_frame = draw_output_frame(np.copy(current_frame), particle_filter.mu)
     output_frame = draw_output_particles(output_frame, particle_filter.particles)
     output_frame = draw_output_mean_particule(output_frame, particle_filter.mu)
     cv2.imshow('Track Cuttlefish', output_frame)
@@ -177,18 +185,19 @@ if __name__ == "__main__":
             quit()
         
         # Resize if video_size is not None
-        if video_size:
-            current_frame = cv2.resize(current_frame, video_size)
+        if args.scale_factor != 1.:
+            current_frame = cv2.resize(current_frame, (int(current_frame.shape[1]/args.scale_factor), int(current_frame.shape[0]/args.scale_factor)))
+
 
         # Perform a pass of the particle filter
-        particle_filter.forward(current_frame, 1/fps)
+        particle_filter.forward(np.copy(current_frame), 1./fps, 1./4.)
 
         # Mean particle
         print(particle_filter.mu)
         print()
 
         # Draw Bbox and particles on the frame
-        output_frame = draw_output_frame(current_frame, particle_filter.mu)
+        output_frame = draw_output_frame(np.copy(current_frame), particle_filter.mu)
         output_frame = draw_output_particles(output_frame, particle_filter.particles)
         output_frame = draw_output_mean_particule(output_frame, particle_filter.mu)
         cv2.imshow('Track Cuttlefish', output_frame)

utils/Similarity.py
@@ -1,10 +1,11 @@
+import cv2
 import numpy as np
 
 # Bhattacharyya distance between a list of descriptors and a template descriptor
 def Bhattacharyya_distance(descriptors: np.ndarray, template: np.ndarray):
     # Mean of the vector descriptor
     descriptors_mean = np.mean(descriptors, axis=1)
-    template_mean = np.mean(template, axis=1)
+    template_mean = np.mean(template)
 
     # Compute the Bhattacharyya coefficient of the descriptor with a template
     bc = np.sum(np.sqrt(descriptors * template), axis=1)
@@ -17,3 +18,15 @@ def Bhattacharyya_distance(descriptors: np.ndarray, template: np.ndarray):
     dist = np.sqrt(np.maximum(0., 1. - (bc * (1./(np.sqrt(descriptors_mean * template_mean * template.shape[1]**2) + np.finfo(float).tiny)))))
 
     return dist
+
+def keypoint_matcher(descriptors: np.ndarray, template: np.ndarray):
+    coeff = []
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    for i, desc in enumerate(descriptors):
+        matches = bf.match(desc[1], template[0][1])
+        if desc and matches:
+            coeff.append(len(matches)/len(desc[1]))
+        else:
+            coeff.append(0)
+
+    return np.array(coeff)

utils/Slicer.py
@@ -11,8 +11,8 @@ def image_resize_slicing(
     sliced_image = []
 
     # Compute padding width and height
-    pad_width = int(np.max(particles[:, :, 6])//2)
-    pad_height = int(np.max(particles[:, :, 7])//2)
+    pad_width = int(np.max(particles[:, :, 6])/2)
+    pad_height = int(np.max(particles[:, :, 7])/2)
 
     # Pad images to take care of Bbox that get outside of the image
     image_pad = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width), (0, 0)), mode='constant', constant_values=0)
@@ -28,7 +28,7 @@ def image_resize_slicing(
         patch = image_pad[y_start:y_end, x_start:x_end]
 
         # Resize the patch to match the size of the template patch
-        final_patch = cv2.resize(patch, (int(template_particle[6]), int(template_particle[7])))
+        final_patch = cv2.resize(patch, (int(template_particle[0, 6]), int(template_particle[0, 7])))
 
         sliced_image.append(final_patch)
 
@@ -43,8 +43,8 @@ def image_crop_slicing(
     sliced_image = []
 
     # Compute padding width and height
-    template_pad_width = int(template_particle[7]//2)
-    template_pad_height = int(template_particle[6]//2)
+    template_pad_width = int(template_particle[0, 6]/2)
+    template_pad_height = int(template_particle[0, 7]/2)
 
     # Pad images to take care of Bbox that get outside of the image
     image_pad = np.pad(image, ((template_pad_height, template_pad_height), (template_pad_width, template_pad_width), (0, 0)), mode='constant', constant_values=0)
@@ -53,8 +53,8 @@ def image_crop_slicing(
         # Compute Start and End point to determine the patch of the image to slice
         x_start = int(p[0, 0])
         y_start = int(p[0, 3])
-        x_end = int(p[0, 0] + template_particle[6])
-        y_end = int(p[0, 3] + template_particle[7])
+        x_end = int(p[0, 0] + template_particle[0, 6])
+        y_end = int(p[0, 3] + template_particle[0, 7])
 
         # Crop the patch to the size of the template patch
         final_patch = image_pad[y_start:y_end, x_start:x_end]
@@ -62,29 +62,6 @@ def image_crop_slicing(
         sliced_image.append(final_patch)
 
     return np.array(sliced_image)
-
-# Get the slice of an image based on the template particle position and Bbox
-def template_image_slicing(
-    template_image: np.ndarray,
-    template_particle: np.ndarray) -> np.ndarray:
-    
-    # Compute padding width and height
-    template_pad_width = int(template_particle[7]//2)
-    template_pad_height = int(template_particle[6]//2)
-
-    # Pad images to take care of Bbox that get outside of the image
-    template_image_pad = np.pad(template_image, ((template_pad_height, template_pad_height), (template_pad_width, template_pad_width), (0, 0)), mode='constant', constant_values=0)
-
-    # Compute Start and End point to determine the patch of the image to slice
-    x_start = int(template_particle[0])
-    y_start = int(template_particle[3])
-    x_end = int(template_particle[0] + template_particle[6])
-    y_end = int(template_particle[3] + template_particle[7])
-
-    # Slice the image based on particles position
-    patch = template_image_pad[y_start:y_end, x_start:x_end]
-
-    return patch
 
 
 #####################################################