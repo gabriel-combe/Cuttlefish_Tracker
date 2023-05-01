a334b04c0f743f9756d928f5b7b3eb8d950d82d5
gabriel_combe <gabrielcombe@gmail.com>
2023-05-01 18:24:41 +0200

    Add new descriptors and similarity fuctions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .Slicer import Resize, Crop
-from .Similarity import Bhattacharyya_distance_sqrt, Bhattacharyya_distance_log, keypoint_matcher
-from .Descriptors import HOG, HOGCOLOR, SIFT, ORB, BRISK, LBP
+from .Similarity import Bhattacharyya_distance_sqrt, Bhattacharyya_distance_log, Cosine_Similarity, Kullback_Leibler_Divergence, keypoint_matcher
+from .Descriptors import HOG, HOGCOLOR, HOGCASCADE, HOGCASCADELBP, SIFT, ORB, BRISK, LBP
 
 slicer_dict = {
     'resize' : Resize,
@@ -10,12 +10,16 @@ slicer_dict = {
 similarity_dict = {
     'bds' : Bhattacharyya_distance_sqrt,
     'bdl' : Bhattacharyya_distance_log,
+    'cos' : Cosine_Similarity,
+    'dkl' : Kullback_Leibler_Divergence,
     'kpm' : keypoint_matcher
 }
 
 descriptor_dict = {
     'hog' : HOG,
     'hogcolor' : HOGCOLOR,
+    'hogcascade' : HOGCASCADE,
+    'hogcascadelbp' : HOGCASCADELBP,
     'lbp' : LBP,
     'sift' : SIFT,
     'orb' : ORB,
