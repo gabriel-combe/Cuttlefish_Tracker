ef6b2f5c2c3044efd6dbe5c0491869b0d273cec3
gabriel_combe <gabrielcombe@gmail.com>
2023-05-01 17:45:01 +0200

    Add cosine similarity and kullback leibler divergence

--- utils/Similarity.py
+++ utils/Similarity.py
@@ -16,11 +16,7 @@ def Bhattacharyya_distance_sqrt(descriptors: np.ndarray, template: np.ndarray):
     # Use this distance if there's issue with floating point number
     # Or if a descriptor full of zeros may occur
     dist = np.sqrt(np.maximum(0., 1. - (bc * (1./(np.sqrt(descriptors_mean * template_mean * template.shape[1]**2) + np.finfo(float).tiny)))))
-    # dist = []
-    # for desc in descriptors:
-    #     dist.append(1 - np.dot(desc, template[0]) / (np.linalg.norm(desc) * np.linalg.norm(template[0])))
 
-    # return np.array(dist)
     return dist
 
 # Bhattacharyya distance between a list of descriptors and a template descriptor
@@ -33,13 +29,32 @@ def Bhattacharyya_distance_log(descriptors: np.ndarray, template: np.ndarray):
     bc = np.sum(np.sqrt(descriptors * template), axis=1)
 
     # Compute the Bhattacharyya distance
-    # dist = np.sqrt(1. - (bc * (1./np.sqrt(descriptors_mean * template_mean * template.shape[0]**2))))
+    # dist = -np.log(bc * (1./np.sqrt(descriptors_mean * template_mean * template.shape[1]**2)))
 
     # Use this distance if there's issue with floating point number
     # Or if a descriptor full of zeros may occur
     dist = -np.log(np.minimum(1., bc * (1./(np.sqrt(descriptors_mean * template_mean * template.shape[1]**2) + np.finfo(float).tiny))) + np.finfo(float).tiny)
 
     return dist
+
+def Cosine_Similarity(descriptors: np.ndarray, template: np.ndarray):
+    descriptors_normalized = descriptors / np.linalg.norm(descriptors, axis=1)[:, np.newaxis]
+    template_normalized = template / np.linalg.norm(template, axis=1)[:, np.newaxis]
+
+    similarity = np.sum(descriptors_normalized*template_normalized, axis=1)
+
+    return 1 - similarity
+
+def Kullback_Leibler_Divergence(descriptors: np.ndarray, template: np.ndarray):
+    indices_nonzero_template = template != 0
+
+    similarity = []
+
+    for desc in descriptors:
+        indices_nonzero_descriptors = desc != 0 & indices_nonzero_template[0]
+        similarity.append(np.sum(desc[indices_nonzero_descriptors] * np.log2(desc[indices_nonzero_descriptors] / template[indices_nonzero_template])))
+
+    return np.array(similarity)
 
 # Keypoint matcher similarity
 def keypoint_matcher(descriptors: np.ndarray, template: np.ndarray):
