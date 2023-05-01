15ce14ef1b8b270193741ac00187486635630faf
gabriel_combe <gabrielcombe@gmail.com>
2023-05-01 17:44:32 +0200

    Add HOG cascade and HOG cascade with LBP

--- utils/Descriptors.py
+++ utils/Descriptors.py
@@ -36,11 +36,86 @@ class HOG():
                 img = cv2.resize(img, self.winSize, interpolation=cv2.INTER_AREA)
             histogram[image] = self.hog.compute(img)
 
+        return histogram
+
+    def update(self, winSize):
+        if not self.freezeSize:
+            self.__init__(winSize, self.blockSize, self.blockStride,  self.cellSize, self.nbins)
+
+class HOGCASCADE():
+
+    def __init__(self, winSize, blockSize = (6, 6), blockStride = (6, 6),  cellSize = (6, 6), nbins = 9, freezeSize = False):
+        self.winSize = (winSize[0] // cellSize[0] * cellSize[0], winSize[1] // cellSize[1] * cellSize[1])
+        self.blockSize = blockSize
+        self.blockStride = blockStride
+        self.cellSize = cellSize
+        self.nbins = nbins
+        self.freezeSize = freezeSize
+        assert((self.winSize[0]-self.blockSize[0])%self.blockStride[0]==0 and (self.winSize[1]-self.blockSize[1])%self.blockStride[1]==0)
+        assert(self.blockSize[0]%self.cellSize[0]==0 and self.blockSize[1]%self.cellSize[1]==0)
+        self.hog = cv2.HOGDescriptor(self.winSize, self.blockSize, self.blockStride, self.cellSize, self.nbins)
+        self.nbcascade = ((max(self.winSize[0], self.winSize[1])//6) - 1) // 2
+
+    def compute(self, images : np.ndarray):
+
+        nbImage = images.shape[0]
+
+        histogram = []
+    
+        for image in range(nbImage):
+            img = cv2.cvtColor(images[image], cv2.COLOR_BGR2GRAY)
+            if self.winSize != img.shape:
+                img = cv2.resize(img, self.winSize, interpolation=cv2.INTER_AREA)
+            
+            desc = []
+            for i in range(self.nbcascade):
+                hogcascade = cv2.HOGDescriptor(self.winSize, (np.minimum(self.blockSize[0]+2*i*self.cellSize[0], self.winSize[0]), np.minimum(self.blockSize[1]+2*i*self.cellSize[0], self.winSize[1])), self.blockStride, self.cellSize, self.nbins)
+                desc.append(hogcascade.compute(img))
+            histogram.append(np.concatenate(desc))
+
         return np.array(histogram)
 
     def update(self, winSize):
         if not self.freezeSize:
             self.__init__(winSize, self.blockSize, self.blockStride,  self.cellSize, self.nbins)
+
+class HOGCASCADELBP():
+
+    def __init__(self, winSize, numPoints, radius, blockSize = (6, 6), blockStride = (6, 6),  cellSize = (6, 6), nbins = 9):
+        self.numPoints = numPoints
+        self.radius = radius
+        self.winSize = (winSize[0] // cellSize[0] * cellSize[0], winSize[1] // cellSize[1] * cellSize[1])
+        self.blockSize = blockSize
+        self.blockStride = blockStride
+        self.cellSize = cellSize
+        self.nbins = nbins
+        assert((self.winSize[0]-self.blockSize[0])%self.blockStride[0]==0 and (self.winSize[1]-self.blockSize[1])%self.blockStride[1]==0)
+        assert(self.blockSize[0]%self.cellSize[0]==0 and self.blockSize[1]%self.cellSize[1]==0)
+        self.nbcascade = ((max(self.winSize[0], self.winSize[1])//6) - 1) // 2
+
+    def compute(self, images : np.ndarray):
+
+        nbImage = images.shape[0]
+
+        histogram = []
+    
+        for image in range(nbImage):
+            img = cv2.cvtColor(images[image], cv2.COLOR_BGR2GRAY)
+            if self.winSize != img.shape:
+                img = cv2.resize(img, self.winSize, interpolation=cv2.INTER_AREA)
+            
+            lbp = feature.local_binary_pattern(img, self.numPoints, self.radius, method="uniform")
+            nbins = int(lbp.max() + 1)
+            desc = [np.histogram(lbp, density=True, bins=nbins, range=(0, nbins))[0]]
+            for i in range(self.nbcascade):
+                hogcascade = cv2.HOGDescriptor(self.winSize, (np.minimum(self.blockSize[0]+2*i*self.cellSize[0], self.winSize[0]), np.minimum(self.blockSize[1]+2*i*self.cellSize[0], self.winSize[1])), self.blockStride, self.cellSize, self.nbins)
+                desc.append(hogcascade.compute(img))
+            histogram.append(np.concatenate(desc))
+
+        return np.array(histogram)
+
+    def update(self, winSize):
+        pass
 
 class HOGCOLOR():
 
@@ -89,10 +164,8 @@ class LBP():
         for image in images:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             lbp = feature.local_binary_pattern(image, self.numPoints, self.radius, method="uniform")
-            hist.append(np.histogram(lbp.ravel(), bins=np.arange(0, self.numPoints + 3), range=(0, self.numPoints + 2))[0])
-
-            hist[-1] = hist[-1].astype("float")
-            hist[-1] /= (hist[-1].sum() + eps)
+            nbins = int(lbp.max() + 1)
+            hist.append(np.histogram(lbp, density=True, bins=nbins, range=(0, nbins))[0])
         
         return np.array(hist)
     
